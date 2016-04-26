c06a102c17e26d2aa3b659e79319391ab16d2faa
igabregu <ivan.abregu@gmail.com>
2016-04-26 13:34:32 -0300

    Cree la clase consultarWebService que implementa un AsyncTask, donde realiza la consulta al web service, carga todos los item en la clase classItem la cual representa un item del web service y luego llama a la función cargarItemsMaps, la cual carga un Marker al mapa por cada ítems de la listItem.

---
+++ app/src/main/java/com/example/igabregu/myapplicationmaps1/classItem.java
@@ -0,0 +1,92 @@
+package com.example.igabregu.myapplicationmaps1;
+
+import org.json.JSONObject;
+
+/**
+ * Created by igabregu on 26/04/2016.
+ * clase que representa un item
+ */
+public class classItem
+{
+
+
+    private  Double locationLatitude;
+    private Double locationLongitude;
+    private int radius;
+    private String code;
+    private int radiusInMeter;
+    private String kind;
+
+
+
+    public classItem(JSONObject json) {
+        try{
+            JSONObject jsLocation = json.getJSONObject("location");
+            this.locationLatitude = jsLocation.getDouble("latitude");
+            this.locationLongitude = jsLocation.getDouble("longitude");
+            this.radius = json.getInt("radius");
+            this.code = json.getString("code");
+            this.radiusInMeter =json.getInt("radiusInMeter");
+            this.kind = json.getString("kind");
+
+        }catch(Exception ex){
+            this.kind="error class "+ex.toString();
+        }
+    }
+
+    public void setKind(String kind)
+    {
+        this.kind = kind;
+    }
+
+    public String getKind()
+    {
+        return kind;
+    }
+
+    public void setRadiusInMeter(int radiusInMeter)
+    {
+        this.radiusInMeter = radiusInMeter;
+    }
+
+    public int getRadiusInMeter()
+    {
+        return radiusInMeter;
+    }
+
+    public void setCode(String code)
+    {
+        this.code = code;
+    }
+
+    public String getCode()
+    {
+        return code;
+    }
+
+    public void setRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public Double getLocationLatitude() {
+        return locationLatitude;
+    }
+
+    public void setLocationLatitude(Double locationLatitude) {
+        this.locationLatitude = locationLatitude;
+    }
+
+    public Double getLocationLongitude() {
+        return locationLongitude;
+    }
+
+    public void setLocationLongitude(Double locationLongitude) {
+        this.locationLongitude = locationLongitude;
+    }
+}
